49f6ef7b4f19034af61b97e60ecf05a79616a63a
Sascha Andeweg <sascha@designed.irish>
2020-03-02 19:22:05 +0000

    changes to navigation, layout and trials js

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -27,3 +27,36 @@ function initMap() {
             {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
 
     }
+
+    /*
+    // JavaScript Document
+
+var locations = [
+     ['An Spailpin Fanach', 3.180967,101.715546, 1],
+     ['Title B', 3.200848,101.616669, 2],
+     ['Title C', 3.147372,101.597443, 3],
+     ['Title D', 3.19125,101.710052, 4]
+];
+var map = new google.maps.Map(document.getElementById('map'), {
+     zoom: 8,
+     center: new google.maps.LatLng(51.967125,-8.933958),
+});
+
+var infowindow = new google.maps.InfoWindow;
+
+var marker, i;
+
+for (i = 0; i < locations.length; i++) {  
+    marker = new google.maps.Marker({
+         position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+         map: map
+    });
+
+    google.maps.event.addListener(marker, 'click', (function(marker, i) {
+         return function() {
+             infowindow.setContent(locations[i][0]);
+             infowindow.open(map, marker);
+         }
+    })(marker, i));
+}
+*/
