b95506147667aebeda16dfd4c437d8e5b74b8ccd
Sascha Andeweg <sascha@designed.irish>
2020-03-09 16:43:25 +0000

    commit of testing js file

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,6 +1,85 @@
 // JavaScript Document
 
-var myJsonArrayFromFile = JSON.parse('http://tradtracker.com/assets.js/pubs.json');
+var dummyLocations = [{
+  "pubCoords": {
+		  "lat": 51.896597,
+		  "lng": -8.476546
+	  },
+  "pubName": "An Spailpin Fanach",
+  "pubAddress": "27-29 South Main Street, Cork City",
+  "pubDescribe": "A character-filled pub in the heart of the city with a warm, Irish atmosphere. Visitors can enjoy traditional music nightly and the Cork Singer’s Club on Sunday evenings. There’s always a bit of great craic in store as well. Food is served Monday through Friday between noon and 3pm.",
+  "pubImg": "http://www.tradtracker.com/assets/img/AnSpailpinFanach.jpg",
+  "pubDays": [
+        "All days",
+        "Sunday",
+        "Monday",
+	  	"Tuesday",
+	  	"Wednesday",
+	  	"Thursday",
+	  	"Friday",
+	  	"Saturday"
+      ]
+    },
+{
+  "pubCoords": {
+		  "lat": 51.623023,
+		  "lng": -8.888975
+	  },
+  "pubName": "DeBarras Folk Club",
+  "pubAddress": "55 Pearse St, Scartagh, Clonakilty",
+  "pubDescribe": "In the last 30 year’s DeBarra’s Folk Club in the beautiful sea-side town of Clonakilty has earned a musical reputation that has traveled far beyond the confines of its West Cork location.",
+  "pubImg": "http://www.tradtracker.com/assets/img/AnSpailpinFanach.jpg",
+  "pubDays": [
+        "",
+        "",
+        "Monday",
+	  	"",
+	  	"",
+	  	"Thursday",
+	  	"",
+	  	""
+      ]
+    },
+{
+  "pubCoords": {
+		  "lat": 52.137655,
+		  "lng": -8.278949
+	  },
+  "pubName": "Cheers Bar",
+  "pubAddress": "81 McCurtain St, Fermoy",
+  "pubDescribe": "live music, live sport on 6 screens, welcoming staff and mighty pints",
+  "pubImg": "http://www.tradtracker.com/assets/img/AnSpailpinFanach.jpg",
+  "pubDays": [
+        "",
+        "",
+        "Monday",
+	  	"",
+	  	"",
+	  	"",
+	  	"",
+	  	""
+      ]
+    },
+{
+  "pubCoords": {
+		  "lat": 51.901743,
+		  "lng": -8.471050
+	  },
+  "pubName": "Sin é",
+  "pubAddress": "8 Coburg St, Victorian Quarter, Cork",
+  "pubDescribe": "The long-established home of Irish traditional music in Cork city, the Irish words ‘Sin é’ literally translate to mean ‘That’s it’, in reference to the funeral parlour located next door. Non-stop traditional music 7 days a week from 6 – close Monday – Saturday, and from 5 on Sunday.",
+  "pubImg": "http://www.tradtracker.com/assets/img/AnSpailpinFanach.jpg",
+  "pubDays": [
+        "All days",
+        "Sunday",
+        "Monday",
+	  	"Tuesday",
+	  	"Wednesday",
+	  	"Thursday",
+	  	"Friday",
+	  	"Saturday"
+      ]
+    }];
 
 function initMap() {
         var map = new google.maps.Map(document.getElementById("map"), {
@@ -63,6 +142,7 @@ if (daysArray[0] > "") {
 	pubLocations[] = myJsonArray[i].pubCoord;
 };
 */
+
 /* 
 function selectPub() {
     switch (document.getElementById("day-all").value) {
@@ -121,23 +201,23 @@ function myredraw(myJsonArrayFromFile) {
  // different coordinates.
  
  if (radio_value == 0) {
-  var myLatLng = {lat: 51.967125, lng: -8.476551};
+  console.log(radio_value);
  } else if (radio_value == 1) {
-  var myLatLng = {lat: 51.967125, lng: -8.476551};
+  console.log(radio_value);
  } else if (radio_value == 2) {
-  var myLatLng = {lat: 51.967125, lng: -8.476551};
+  console.log(radio_value);
  } else if (radio_value == 3) {
-  var myLatLng = {lat: 51.967125, lng: -8.476551};
+  var my  console.log(radio_value);
  } else if (radio_value == 4) {
-  var myLatLng = {lat: 51.967125, lng: -8.476551};
+  console.log(radio_value);
  } else if (radio_value == 5) {
-  var myLatLng = {lat: 51.967125, lng: -8.476551};
+  console.log(radio_value);
  } else if (radio_value == 6) {
-  var myLatLng = {lat: 51.967125, lng: -8.476551};
+  console.log(radio_value);
  } else if (radio_value == 7) {
-  var myLatLng = {lat: 51.967125, lng: -8.476551};
+  console.log(radio_value);
  } else {
-  var myLatLng = {lat: 51.967125, lng: -8.476551};
+  console.log(radio_value);
  }
 
  
