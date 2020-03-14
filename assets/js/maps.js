058ca9f68c3cdbf381956e78817559b78bb18572
Sascha Andeweg <sascha@designed.irish>
2020-03-14 13:46:41 +0000

    moved back to the external json file and working on fetching pubdata now

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,90 +1,5 @@
 // JavaScript Document
-/*
-var hardcodeLocations = [{
-    "pubCoords": {
-        "lat": 51.896597,
-        "lng": -8.476546
-    },
-    "pubName": "An Spailpin Fanach",
-    "pubAddress": "27-29 South Main Street, Cork City",
-    "pubDescribe": "A character-filled pub in the heart of the city with a warm, Irish atmosphere. Visitors can enjoy traditional music nightly and the Cork Singer’s Club on Sunday evenings. There’s always a bit of great craic in store as well. Food is served Monday through Friday between noon and 3pm.",
-    "pubImg": "http://www.tradtracker.com/assets/img/AnSpailpinFanach.jpg",
-    "pubDays": [
-        "All days",
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday"
-    ]
-},
-{
-    "pubCoords": {
-        "lat": 51.623023,
-        "lng": -8.888975
-    },
-    "pubName": "DeBarras Folk Club",
-    "pubAddress": "55 Pearse St, Scartagh, Clonakilty",
-    "pubDescribe": "In the last 30 year’s DeBarra’s Folk Club in the beautiful sea-side town of Clonakilty has earned a musical reputation that has traveled far beyond the confines of its West Cork location.",
-    "pubImg": "http://www.tradtracker.com/assets/img/AnSpailpinFanach.jpg",
-    "pubDays": [
-        "",
-        "",
-        "Monday",
-        "",
-        "",
-        "Thursday",
-        "",
-        ""
-    ]
-},
-{
-    "pubCoords": {
-        "lat": 52.137655,
-        "lng": -8.278949
-    },
-    "pubName": "Cheers Bar",
-    "pubAddress": "81 McCurtain St, Fermoy",
-    "pubDescribe": "live music, live sport on 6 screens, welcoming staff and mighty pints",
-    "pubImg": "http://www.tradtracker.com/assets/img/AnSpailpinFanach.jpg",
-    "pubDays": [
-        "",
-        "",
-        "Monday",
-        "",
-        "",
-        "",
-        "",
-        ""
-    ]
-},
-{
-    "pubCoords": {
-        "lat": 51.901743,
-        "lng": -8.471050
-    },
-    "pubName": "Sin é",
-    "pubAddress": "8 Coburg St, Victorian Quarter, Cork",
-    "pubDescribe": "The long-established home of Irish traditional music in Cork city, the Irish words ‘Sin é’ literally translate to mean ‘That’s it’, in reference to the funeral parlour located next door. Non-stop traditional music 7 days a week from 6 – close Monday – Saturday, and from 5 on Sunday.",
-    "pubImg": "http://www.tradtracker.com/assets/img/AnSpailpinFanach.jpg",
-    "pubDays": [
-        "All days",
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday"
-    ]
-}];
 
-*/
-
-
-// keeping the initMap for later to use in the radiobutton myRedraw function
 function initMap() {
         var map = new google.maps.Map(document.getElementById("map"), {
             zoom: 9,
@@ -114,18 +29,21 @@ function initMap() {
             {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
 
     }
-var allPubs;
+var allPubs=[];
 
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-       // document.getElementById("demo").innerHTML = this.responseText;
+        document.getElementById("demo").innerHTML = this.responseText;
         var allPubs = JSON.parse(this.responseText);
+        console.log(allPubs.data);
     }
 };
 
 xhttp.open("GET", "../assets/js/pubs.json", true);
 xhttp.send();
+
+console.log(allPubs);
 
 var text1 = "";
 var i;
